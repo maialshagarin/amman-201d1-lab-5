bb7fc9e727efac9ec9f8651b0c645399ed65a2d7
mai <mai.yusuf28@yahoo.com>
2019-10-12 01:35:16 +0300

    problem6

--- app.js
+++ app.js
@@ -145,10 +145,15 @@ Test this function by hand in the console to get it working, and when you think 
 var testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
+    var complexMult1 = multiply(testDynamicArray[0],testDynamicArray[1]);
+    var complexMult2 = multiply(complexMult1[0] , testDynamicArray[2]);
+    var complexMult3 = multiply(complexMult2[0] ,testDynamicArray[3]);
+    var complexMult4 = multiply(complexMult3[0],testDynamicArray[4]);
+    return [complexMult4[0], "The numbers 1,2,3,4,5 have a product of 120."]
 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+ testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
